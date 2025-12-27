e22bdfe398e8bdbf148f86c51cc4514fd6391547
Adisak Wongwitayapitak <adisakwong@gmail.com>
2025-12-27 17:54:06 +0700

    Update script.js
    edt12

--- script.js
+++ script.js
@@ -152,15 +152,15 @@ function renderUI(hadith, thaiText) {
 }
 
 
-
 // ฟังก์ชันแสดง Notification เล็กๆ
-// function showToast(message) {
-//     const toast = document.createElement("div");
-//     toast.className = "toast";
-//     toast.innerText = message;
-//     document.body.appendChild(toast);
-//     setTimeout(() => toast.remove(), 2500);
-// }
+function showToast(message) {
+    const toast = document.createElement("div");
+    toast.className = "toast";
+    toast.innerText = message;
+    document.body.appendChild(toast);
+    setTimeout(() => toast.remove(), 2500);
+}
+
 
 
 
